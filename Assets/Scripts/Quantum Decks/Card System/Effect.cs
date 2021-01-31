501540e69713bcf3b963f7099b108524cf118f7b
Raphael Kölz <raphael.koelz@gmx.net>
2021-01-31 10:16:10 +0100

    Localization Ids added

--- Assets/Scripts/Quantum Decks/Card System/Effect.cs
+++ Assets/Scripts/Quantum Decks/Card System/Effect.cs
@@ -5,6 +5,8 @@ namespace Quantum_Decks.Card_System
 {
     public abstract class Effect : ScriptableObject
     {
+        public string NameId;
+        
         public abstract IEnumerator ApplyEffect(Player.Player player);
     }
 }
